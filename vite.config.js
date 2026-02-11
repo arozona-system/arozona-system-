bf08503f1f617584cd44e4369e4ce1d6a53c0753
arozona <mellyna.lambotte@providencechampion.be>
2026-02-11 21:06:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // <-- dit à Vite que index.html est à la racine
+  root: '.', 
   build: {
     outDir: 'dist'
   }
